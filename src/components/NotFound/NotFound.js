ca4492795f69a01fca226b1568fa3780bbb3c394
Hossain <maherhossain10@gmail.com>
2021-10-31 23:28:49 +0600

    update 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
-      <p>not found</p>
+    <div className="container">
+      <div className="row my-5">
+        <h2 className="text-center my-3">Page Not Found</h2>
+        <img className="img-fluid mx-auto w-50" src="https://techhossain.github.io/api/not-found.jpg" alt="" />
+      </div>
     </div>
   );
 };
